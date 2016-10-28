2e67f8dec69939ffeae06cd909f30c62e6deefd5
Nic Schumann <nicolas_schumann@brown.edu>
2016-10-28 11:27:36 -0400

    Bridge: renamed and added acf fields.

--- dynamic/utilities/destructure-projects-response.js
+++ dynamic/utilities/destructure-projects-response.js
@@ -7,6 +7,8 @@ module.exports = function ( r ) {
 
     try {
 
+        console.log( r.acf );
+
         var x = {
             title: maybeRendered( r.title ).rendered,
             slug: r.slug,
@@ -15,7 +17,15 @@ module.exports = function ( r ) {
             id: r.id,
             link: r.link,
             featured_media: maybeSourceUrl( r._embedded )['wp:featuredmedia'][0].source_url,
-            content: maybeRendered( r.content ).rendered
+            content: maybeRendered( r.content ).rendered,
+            description: {
+                short: r.acf.description,
+                long:  r.acf.description_long,
+            },
+            client: r.acf.client,
+            timeline: r.acf.timeline,
+            services: r.acf.services,
+            gallery: r.acf.gallery
         };
 
         return x;
