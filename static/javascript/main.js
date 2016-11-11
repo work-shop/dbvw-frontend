99563b93e61a3a6f7e82d195a3744a48c430d4df
Greg Nemes <mail@gregnemes.com>
2016-11-11 12:24:49 -0500

    added sass files, got javascript jump links working

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,13 +1,22 @@
+"use strict";
 
-
+//configuration
 var configuration = require('../../package.json').frontend;
 
+
+//third party libraries
 var $ = require('jquery');
 
+
+//utilities
 var jumpUtilities = require('./jump-utilities.js')($);
 
+
+//site
 var slideshows = require('./slideshows.js')($);
 
 
 
+//setup
 jumpUtilities.setupJumpEvents('.jump');
+
