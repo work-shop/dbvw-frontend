e552ac2a8ff97259a23edb2311d8a47fed4cbfef
Greg Nemes <mail@gregnemes.com>
2017-01-24 15:47:08 -0500

    removed equals sign from person url

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -38,10 +38,10 @@ awardsToggle.setupAwardsToggle();
 
 //page specific
 if($('body').hasClass('page-about')){
-	//var scrollSpy = require('./scroll-spy.js')($);	
-	//scrollSpy.initialize('.spy-start', '.spy-target', '.spy-link', 135);
-	//var aboutNav = require('./about-nav.js')($);
-	//aboutNav.setupAboutNav();
+	var scrollSpy = require('./scroll-spy.js')($);	
+	scrollSpy.initialize('.spy-start', '.spy-target', '.spy-link', 135);
+	var aboutNav = require('./about-nav.js')($);
+	aboutNav.setupAboutNav();
 }
 
 
