bc6673b0076fa4817b40aae1b1a1578d6e2eccda
Greg Nemes <mail@gregnemes.com>
2017-02-09 19:47:57 -0500

    significant progress on search

--- static/javascript/search.js
+++ static/javascript/search.js
@@ -1,30 +1,42 @@
 "use strict";
 
+var _ = require('underscore');
+var async = require('async');
+
 module.exports = function( $, configuration ) {
 
 	var base = configuration.remote_api;
-	var responses = [];
 	var responseCount;
+	var resultsCount;	
 	var searching = false;
-	var totalResponses = 1;
+	var totalResponses = 2;
 	var $searchButton = $('#search-button');
 	var $searchInput = $('#search-input');
 	var $searchInformationMessage = $('#search-information-message');
-
+	var $resultsContainer = $('#search-results');
+	var $categoriesContainer = $('#results-categories');
+	var $projectsContainer = $('#results-projects');
+	var $newsContainer = $('#results-news');
+	var $infoContainer = $('#results-info');	
+	var $aboutContainer = $('#results-about');
+	var m1 = '<div class="col-xs-12 search-result"><a href="';
+	var m3 = '" ><h4>';
+	var m5 = '</h4><h3>';
+	var m7 = '</h3></a></div>';
 
 
 	//initialize search processes
 	function initialize(){
-		
+
 		console.log('initialize search');
 
 		$( document ).ready( function() {
 
-			searchToggle();
+			//searchModalToggle();
 
 			$(".search-toggle").click(function(e){
 				e.preventDefault();
-				searchToggle();	
+				searchModalToggle();	
 			});
 
 			$searchButton.click(function( event ) {
@@ -50,58 +62,19 @@ module.exports = function( $, configuration ) {
 	}
 
 
-	function searchToggle(){
+	function searchModalToggle(){
 
-		console.log('search toggle');
+		console.log('search modal toggle');
 
 		if($('body').hasClass('search-off')){
-			$('#search-modal').removeClass('off').addClass('on');
+			$('#search').removeClass('off').addClass('on');
 			$('body').removeClass('search-off').addClass('search-on');
 			$('#search-input').focus();
 		} else {
-			$('#search-modal').removeClass('on').addClass('off');
+			$('#search').removeClass('on').addClass('off');
 			$('body').removeClass('search-on').addClass('search-off');	
 			$('body').removeClass('search-after').addClass('search-before');		
 		}	
-
-	}
-
-
-	function validate(){
-
-		var query = $searchInput.val();
-
-		if( typeof query !== 'undefined' && query.length >= 2 ){
-			search(query);
-		} else{
-			console.log('search query did not validate');
-		}
-
-	}
-
-
-	function search( query ){
-		console.log('search for: ' + query);
-
-		responseCount = 0;
-		responses = [];
-		toggleSearchState();
-
-		var requests = [
-		'/projects?search=' + query
-		];
-
-		for (var i in requests) {
-			var term = requests[i];
-			searchRequest( term );
-		}
-
-
-		//info pages
-		//categories
-		//projects
-		//news
-		//jobs
 
 	}
 
@@ -122,12 +95,63 @@ module.exports = function( $, configuration ) {
 			console.log('searching');									
 		}
 
+	}	
+
+
+	function validate(){
+
+		var query = $searchInput.val();
+
+		if( typeof query !== 'undefined' && query.length >= 2 ){
+			search(query);
+		} else{
+			console.log('search query did not validate');
+		}
+
 	}
 
 
-	function searchRequest( term ){
+	function search( query ){
+		
+		console.log('search for: ' + query);
+		responseCount = 0;	
+		resultsCount = 0;
+		$('.search-result').remove();	
+		toggleSearchState();
 
-		console.log('searchRequest for: ' + term)
+		//projects request 1
+		// var projectRequests = [
+		// '/projects?search=' + query,
+		// '/projects?filter[meta_key]=short_description&filter[meta_value]=' + query + '&filter[meta_compare]=LIKE'
+		// ];
+		//projects request 2
+		var categoriesRequest = [
+		'/project_categories?search=' + query
+		];		
+		async.map( categoriesRequest, searchRequest, function(err, results) {
+			console.log('categoriesRequests returned');
+			var categoriesResults = concatResults( results );
+			categoriesResults = unionResults( categoriesResults );
+			renderSearchResults( categoriesResults, 'category' );
+		});
+
+		var projectRequests = [
+		'/projects?search=' + query
+		];		
+		async.map( projectRequests, searchRequest, function(err, results) {
+			console.log('projectRequests returned');
+			var projectResults = concatResults( results );
+			projectResults = unionResults( projectResults );
+			renderSearchResults( projectResults, 'projects' );
+		});				
+
+	}
+
+
+
+	function searchRequest( term, callback ){
+
+		console.log('searchRequest for: ' + term);
 
 		var endpoint = base + term;
 
@@ -136,42 +160,71 @@ module.exports = function( $, configuration ) {
 			dataType: 'json'
 		})
 		.done(function(data) {
-			console.log("successful search request");
-			recordResponse(true, data);
-			console.log(data);
+			callback( null, data );
 		})
-		.fail(function() {
-			console.log("error on search request");
-			recordResponse(false);
+		.fail(function( err ) {
+			//console.log("error on search request");
+			console.error( err );
+			callback( new Error('Could not resolve search request') );
 		})
-		.always(function() {
-			console.log("completed search request");
+		.always(function() { 
+			//console.log("completed search request");
 		});
 		
 	}
 
-	function recordResponse( status, data ){
+
+	function renderSearchResults( results, resultType ){
 
 		responseCount++;
+		resultsCount += results.length;
 
-		if( responses === totalResponses ){
-			
+		for (var i = 0; i < results.length; i++) {
+			generateMarkup( results[i], resultType );
+		}
+
+		if( responseCount === totalResponses ){
 			console.log('all responses returned');
-			
-			setTimeout(function() { 
-				toggleSearchState(); 
-				$searchInformationMessage.text('results found');
-
-			}, 3000);
-			
+			toggleSearchState(); 
+			$searchInformationMessage.text(resultsCount + ' results found');
 		}
 
 	}
 
 
-	function renderSearchResults( data, success ){
+	function concatResults( results ){
+		var concatResults = results.reduce( function( a,b ) {
+			return a.concat( b );
+		}, []);
+		return concatResults;
+	}
 
 
+	function unionResults( results ){
+		var unionResults = _.uniq(results, function( project ){
+			return project.id;
+		});
+		return unionResults;
+	}
+
+
+	function generateMarkup( result, type ){
+		var m2,m4,m6;
+
+		if( type === 'category'){
+			m2 = '/work/' + result.slug;
+			m6 = result.name;
+		} else {
+			m2 = result.link;
+			m6 = result.title.rendered;
+		}
+		var m4 = type;
+
+		var markup = m1+m2+m3+m4+m5+m6+m7;
+
+		setTimeout(function() {
+			$projectsContainer.append( markup );
+		}, 10);
 
 	}
 
